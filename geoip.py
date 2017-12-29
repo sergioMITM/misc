a77c1b82ac6eb8ae208e018bdf6c404c4178cb92
sergioMITM <34415716+sergioMITM@users.noreply.github.com>
2017-12-29 16:55:01 +0100

    update geoip
    add utf encoding. #simplify

--- geoip.py
+++ geoip.py
@@ -3,36 +3,27 @@ import csv, urllib, json
 infile=open("users.csv", 'rb')
 reader = csv.reader(infile)
 
-outfile = open("users_geoip.py","wb")
+outfile = open("users_geoip.csv","wb")
 writer = csv.writer(outfile)
 
-data = []
 url = "http://freegeoip.net/json/"
 for r in reader:
 	d=[]
 	d.append(r[0])
 	d.append(r[1])
 	d.append(r[2])
-	d.append(r[3])
 	u = url + r[0]
 	try:
 		response = urllib.urlopen(u)
 		j = json.loads(response.read())
-		d.append(j['country_name'])
-		d.append(j['city'])
+		d.append(j['country_name'].encode('utf-8'))
+		d.append(j['city'].encode('utf-8'))
 		d.append(j['longitude'])
 		d.append(j['latitude'])
 	except Exception as e:
 		print e
-	data.append(d)
 	print d
-
-for d in data:
 	writer.writerow(d)
 
 f.close()
 outfile.close()
-
-
-
-
